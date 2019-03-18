10d95748c834ebc8b19a87a0da6ff29340eaeed8
Willis Pinaud <willis@livelikevr.com>
2019-03-18 18:17:21 +0100

    SDK-225: Get image from UI thread (Glide's requirement)

--- livelikesdk/src/main/java/com/livelike/livelikesdk/widget/view/AlertWidget.kt
+++ livelikesdk/src/main/java/com/livelike/livelikesdk/widget/view/AlertWidget.kt
@@ -6,6 +6,7 @@ import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat.startActivity
 import android.util.AttributeSet
@@ -69,9 +70,11 @@ class AlertWidget : ConstraintLayout {
             bodyText.requestLayout()
         } else {
             resourceAlert.image_url.apply {
-                Glide.with(context)
-                    .load(resourceAlert.image_url)
-                    .into(bodyImage)
+                Handler(Looper.getMainLooper()).post {
+                    Glide.with(context)
+                        .load(resourceAlert.image_url)
+                        .into(bodyImage)
+                }
             }
         }
 
