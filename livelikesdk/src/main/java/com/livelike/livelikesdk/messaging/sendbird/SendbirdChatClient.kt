158ee0994cb92087c34fe5be995b8c150a83708b
Shane Kerr <shane.kerr@teamspace.ca>
2019-03-25 15:58:35 -0400

    SDK-134 - Fix for 50+ issue

--- livelikesdk/src/main/java/com/livelike/livelikesdk/messaging/sendbird/SendbirdChatClient.kt
+++ livelikesdk/src/main/java/com/livelike/livelikesdk/messaging/sendbird/SendbirdChatClient.kt
@@ -2,8 +2,8 @@ package com.livelike.livelikesdk.messaging.sendbird
 
 import com.livelike.livelikesdk.messaging.ClientMessage
 import com.livelike.livelikesdk.util.gson
+import com.livelike.livelikesdk.util.logDebug
 import com.livelike.livelikesdk.util.logError
-import com.sendbird.android.BaseChannel
 import com.sendbird.android.OpenChannel
 import com.sendbird.android.UserMessage
 import org.threeten.bp.Instant
@@ -43,19 +43,19 @@ internal class SendbirdChatClient : ChatClient {
 
     override fun updateMessagesSinceMessage(messageId: String, channel: String) {
         OpenChannel.getChannel(channel) { openChannel, exception ->
-            openChannel.getNextMessagesById(messageId.toLong(),
-                false,
-                SendbirdMessagingClient.CHAT_HISTORY_LIMIT,
-                false,
-                BaseChannel.MessageTypeFilter.ALL,
-                "",
-                BaseChannel.GetMessagesHandler { list, e ->
-                    if (e != null) {
-                        logError { e }
-                        return@GetMessagesHandler
-                    }
-                    messageHandler?.handleMessages( list.map { SendBirdUtils.clientMessageFromBaseMessage(it as UserMessage, openChannel) })
+            openChannel.createPreviousMessageListQuery().load(
+                SendbirdMessagingClient.CHAT_HISTORY_LIMIT, false) { list, e ->
+                if (e != null) {
+                    logError { e }
+                    return@load
+                }
+                messageHandler?.handleMessages(list.takeLastWhile { it.messageId.toString() > messageId }.map {
+                    SendBirdUtils.clientMessageFromBaseMessage(
+                        it as UserMessage,
+                        openChannel
+                    )
                 })
+            }
         }
     }
 }
