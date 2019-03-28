8466dfc05b2a2b27443c1fd5e75244803d8a5984
Shane Kerr <shane.kerr@teamspace.ca>
2019-03-28 14:44:08 -0400

    SDK-222 - Change text prediction layout to RV implementation for consistency and ease of reuse

--- engagementsdkcore/src/main/java/com/livelike/livelikesdk/widget/view/prediction/text/PredictionTextWidgetBase.kt
+++ engagementsdkcore/src/main/java/com/livelike/livelikesdk/widget/view/prediction/text/PredictionTextWidgetBase.kt
@@ -4,31 +4,27 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
-import android.support.v4.content.ContextCompat
-import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.content.res.AppCompatResources
+import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewStub
 import android.widget.Button
-import android.widget.TextView
 import com.livelike.livelikesdk.R
 import com.livelike.livelikesdk.animation.ViewAnimation
 import com.livelike.livelikesdk.binding.WidgetObserver
-import com.livelike.livelikesdk.util.AndroidResource.Companion.dpToPx
-import com.livelike.livelikesdk.util.logDebug
 import com.livelike.livelikesdk.widget.model.VoteOption
 import kotlinx.android.synthetic.main.confirm_message.view.*
 import kotlinx.android.synthetic.main.prediction_text_widget.view.*
+import kotlinx.android.synthetic.main.text_option_row_element.view.*
 
 open class PredictionTextWidgetBase : ConstraintLayout, WidgetObserver {
     protected val widgetOpacityFactor: Float = 0.2f
     protected val constraintSet = ConstraintSet()
     protected val buttonList: ArrayList<Button> = ArrayList()
-    protected val buttonMap = mutableMapOf<Button, String>()
+    protected val buttonMap = mutableMapOf<Button, String?>()
     protected var optionSelected = false
     protected var layout = ConstraintLayout(context, null, 0)
     protected var lottieAnimationPath = ""
@@ -72,9 +68,7 @@ open class PredictionTextWidgetBase : ConstraintLayout, WidgetObserver {
     }
 
     override fun optionListUpdated(voteOptions: List<VoteOption>, optionSelectedCallback: (String?) -> Unit, correctOptionWithUserSelection: Pair<String?, String?>) {
-        val textView = findViewById<TextView>(R.id.prediction_question_textView)
-        addNewlyCreatedButtonsToLayout(context, voteOptions, optionSelectedCallback)
-        applyConstraintsBetweenViews(constraintSet, textView)
+        option_list.adapter = TextOptionAdapter(voteOptions, optionSelectedCallback)
     }
 
     override fun optionSelectedUpdated(selectedOptionId: String?) {
@@ -85,86 +79,44 @@ open class PredictionTextWidgetBase : ConstraintLayout, WidgetObserver {
         }
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun addNewlyCreatedButtonsToLayout(
-        context: Context,
-        voteOptions: List<VoteOption>,
-        optionSelectedCallback: (String?) -> Unit) {
-        voteOptions.forEachIndexed{ index, option ->
-            val button = Button(context)
-            buttonList.add(button)
-            if (option.id != null) {
-                applyStyle(button, option.description , context, voteOptions, index)
-                buttonMap[button] = option.id
-                button.setOnClickListener {
-                    optionSelected = true
-                    button.text.also {
-                        logDebug { "Option selected: $it" }
-                        val selectedButton = buttonMap[button]
-                        optionSelectedCallback(selectedButton)
-                        userTapped.invoke()
-                    }
-                }
-                viewAnimation.addHorizontalSwipeListener(button, layout, dismissWidget)
-            }
-            layout.addView(button)
-        }
-    }
-
     protected fun dismissWidget() {
         dismissWidget?.invoke()
     }
 
-    private fun applyStyle(button: Button,
-                           buttonText: String,
-                           context: Context,
-                           voteOptions: List<VoteOption>,
-                           index: Int) {
-        button.apply {
-            background = if (isLastButtonToBeAddedToLayout(voteOptions, index)) {
-                AppCompatResources.getDrawable(context, R.drawable.bottom_rounded_corner)
-            } else AppCompatResources.getDrawable(context, R.drawable.button_default)
-            setTextColor(ContextCompat.getColor(context, R.color.text_color))
-            text = buttonText
-            layoutParams = LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT)
-            elevation = dpToPx(4).toFloat()
-            gravity = Gravity.START; Gravity.CENTER_VERTICAL
-            id = View.generateViewId()
+    inner class TextOptionAdapter(
+        private val optionList: List<VoteOption>,
+        private val optionSelectedCallback: (String?) -> Unit) : RecyclerView.Adapter<ViewHolder>() {
 
-            typeface = ResourcesCompat.getFont(context, R.font.titillium_semibold)
-            textSize = 15f
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val option = optionList[position]
+            val button = holder.optionButton
+
+            button.text = option.description
+            buttonMap[button] = option.id
+            button.setOnClickListener {
+                optionSelected = true
+                val selectedButton = buttonMap[button]
+                optionSelectedCallback(selectedButton)
+                    userTapped.invoke()
+            }
+            if(position == optionList.lastIndex)
+                button.background = AppCompatResources.getDrawable(context, R.drawable.bottom_rounded_corner)
+
+            viewAnimation.addHorizontalSwipeListener(button, layout, dismissWidget)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.text_option_row_element, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return optionList.size
         }
     }
 
-    private fun applyConstraintsBetweenViews(constraintSet: ConstraintSet,
-                                             textView: TextView) {
-        constraintSet.clone(layout)
-        val viewIds = ArrayList<Int>()
-        val viewWeights = ArrayList<Float>()
-
-        val weightToBeAppliedToEachView = 1.0f
-        viewWeights.add(weightToBeAppliedToEachView)
-        viewIds.add(textView.id)
-
-        for (button in buttonList) {
-            viewIds.add(button.id)
-            viewWeights.add(weightToBeAppliedToEachView)
-        }
-        constraintSet.setVerticalBias(textView.id, 0f)
-        constraintSet.createVerticalChain(
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.TOP,
-                ConstraintSet.PARENT_ID,
-                ConstraintSet.BOTTOM,
-                viewIds.toIntArray(),
-                viewWeights.toFloatArray(),
-                ConstraintSet.CHAIN_PACKED)
-
-        constraintSet.applyTo(layout)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val optionButton: Button = view.text_button
     }
-
-    private fun isLastButtonToBeAddedToLayout(options: List<VoteOption>, index: Int) =
-        options[index] == options[options.size - 1]
 }
