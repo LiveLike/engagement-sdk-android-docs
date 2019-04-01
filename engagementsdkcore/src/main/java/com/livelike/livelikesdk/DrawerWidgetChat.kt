7c43c6932a7baf1d9fb894a6a8b79e561e033884
Willis Pinaud <willis@livelikevr.com>
2019-04-01 16:10:17 +0200

    SDK-76: Add orientable chevron

--- engagementsdkcore/src/main/java/com/livelike/livelikesdk/DrawerWidgetChat.kt
+++ engagementsdkcore/src/main/java/com/livelike/livelikesdk/DrawerWidgetChat.kt
@@ -8,11 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.RelativeLayout
 import com.livelike.livelikesdk.chat.ChatView
+import com.livelike.livelikesdk.slidinglayer.SlidingLayer
 import com.livelike.livelikesdk.slidinglayer.SlidingLayer.STICK_TO_BOTTOM
 import com.livelike.livelikesdk.slidinglayer.SlidingLayer.STICK_TO_LEFT
 import com.livelike.livelikesdk.slidinglayer.SlidingLayer.STICK_TO_RIGHT
 import com.livelike.livelikesdk.slidinglayer.SlidingLayer.STICK_TO_TOP
 import com.livelike.livelikesdk.util.AndroidResource.Companion.dpToPx
+import com.livelike.livelikesdk.util.logError
 import com.livelike.livelikesdk.widget.view.WidgetView
 import kotlinx.android.synthetic.main.drawer_chat_widget.view.*
 
@@ -20,6 +22,7 @@ class DrawerWidgetChat(context: Context, attrs: AttributeSet?) : ConstraintLayou
     var chat: ChatView
     var widgets: WidgetView
     var previewSize: Int
+    var orientation: Int = STICK_TO_RIGHT
 
     init {
         val viewRoot: View = LayoutInflater.from(context).inflate(R.layout.drawer_chat_widget, this, true)
@@ -40,11 +43,70 @@ class DrawerWidgetChat(context: Context, attrs: AttributeSet?) : ConstraintLayou
         view.setBackgroundColor(ta.getColor(R.styleable.DrawerWidgetChat_chat_background_color, Color.TRANSPARENT))
 
         view.setChangeStateOnTap(ta.getBoolean(R.styleable.DrawerWidgetChat_change_state_on_tap, true))
+
+        view.setOnInteractListener(object : SlidingLayer.OnInteractListener {
+            override fun onOpened() {
+                logError { "onOpened" }
+            }
+
+            override fun onClosed() {
+                logError { "onClosed" }
+                view.openPreview(true)
+            }
+
+            override fun onOpen() {
+                logError { "onOpen" }
+                chevron.alpha = 1f
+                when (orientation) {
+                    STICK_TO_BOTTOM -> {
+                        chevron.rotation = -90f
+                    }
+                    STICK_TO_TOP -> {
+                        chevron.rotation = 90f
+                    }
+                    STICK_TO_RIGHT -> {
+                        chevron.rotation = 180f
+                    }
+                    STICK_TO_LEFT -> {
+                        chevron.rotation = 0f
+                    }
+                }
+            }
+
+            override fun onShowPreview() {
+                logError { "onClose" }
+                chevron.alpha = 0.2f
+                when (orientation) {
+                    STICK_TO_BOTTOM -> {
+                        chevron.rotation = 90f
+                    }
+                    STICK_TO_TOP -> {
+                        chevron.rotation = -90f
+                    }
+                    STICK_TO_RIGHT -> {
+                        chevron.rotation = 0f
+                    }
+                    STICK_TO_LEFT -> {
+                        chevron.rotation = 180f
+                    }
+                }
+            }
+
+            override fun onClose() {
+                view.openPreview(true)
+            }
+
+            override fun onPreviewShowed() {}
+
+
+        })
+
         view.openLayer(false) // open drawer immediately
     }
 
     private fun setDrawerOrientation(orientation: Int) {
         view.setStickTo(orientation)
+        this.orientation = orientation
 
         (chatView.layoutParams as RelativeLayout.LayoutParams).removeRule(RelativeLayout.BELOW)
         (chatView.layoutParams as RelativeLayout.LayoutParams).removeRule(RelativeLayout.ABOVE)
