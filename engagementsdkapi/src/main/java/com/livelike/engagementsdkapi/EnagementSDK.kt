2a7daec3467ae8855847606ddcd53a08f061aefd
Willis Pinaud <willis@livelikevr.com>
2019-04-15 12:38:02 +0200

    SDK-245: Document EpochTime

--- engagementsdkapi/src/main/java/com/livelike/engagementsdkapi/EnagementSDK.kt
+++ engagementsdkapi/src/main/java/com/livelike/engagementsdkapi/EnagementSDK.kt
@@ -1,5 +1,11 @@
 package com.livelike.engagementsdkapi
 
+/**
+ * EpochTime represents a timestamp.
+ * It is used by the library to synchronize the session's events with the video across all users.
+ *
+ * @property timeSinceEpochInMs Number of milliseconds that have elapsed since 00:00:00 Thursday, 1 January 1970 UTC
+ */
 class EpochTime(val timeSinceEpochInMs: Long) : Comparable<EpochTime> {
     override fun compareTo(other: EpochTime): Int {
         return timeSinceEpochInMs.compareTo(other.timeSinceEpochInMs)
