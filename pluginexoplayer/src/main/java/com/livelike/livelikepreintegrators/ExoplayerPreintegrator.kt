606f5acfb7882a26d9b2a39f60e82ca19eeb41ae
Willis Pinaud <willis@livelikevr.com>
2019-04-16 13:33:08 +0200

    SDK-245: Document Plugin Exoplayer

--- pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegrator.kt
+++ pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegrator.kt
@@ -5,6 +5,13 @@ import com.google.android.exoplayer2.Timeline
 import com.livelike.engagementsdkapi.LiveLikeContentSession
 import com.livelike.livelikesdk.LiveLikeSDK
 
+/**
+ * This extension act as a plugin for Exoplayer.
+ * It retrieves the timecode from an HLS stream using the PDT tags.
+ *
+ * @param player A lambda returning the latest player instance when called.
+ * @param contentId The Content Session is to connect the created session to the LiveLike CMS.
+ */
 fun LiveLikeSDK.createExoplayerSession(
     player: () -> SimpleExoPlayer?,
     contentId: String
