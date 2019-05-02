a7ba0115bd1ce9fdaa376095a07bbdc1c4116028
Willis Pinaud <willis@livelikevr.com>
2019-05-02 12:16:40 +0200

    SDK-248: Create exoplayer extension for easy Java integration

--- pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegratorJava.kt
+++ pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegratorJava.kt
@@ -9,22 +9,20 @@ import com.livelike.livelikesdk.LiveLikeSDK
  * This extension act as a plugin for Exoplayer.
  * It retrieves the timecode from an HLS stream using the PDT tags.
  *
- * @param player A lambda returning the latest player instance when called.
+ * @param playerGetter An interface returning the latest player instance when called.
  * @param programId The program to connect the session with. This Id can be created from the Engagement CMS.
  */
-@JvmSynthetic
 fun LiveLikeSDK.createExoplayerSession(
-    player: () -> SimpleExoPlayer?,
+    playerGetter: PlayerGetter,
     programId: String
 ): LiveLikeContentSession {
     return this.createContentSession(programId) {
-        getExoplayerPdtTime(player)
+        getExoplayerPdtTime(playerGetter)
     }
 }
 
-@JvmSynthetic
-fun getExoplayerPdtTime(player: () -> SimpleExoPlayer?): Long {
-    return player()?.let {
+fun getExoplayerPdtTime(playerGetter: PlayerGetter): Long {
+    return playerGetter.getPlayer()?.let {
         it.currentTimeline?.run {
             if (!isEmpty) {
                 getWindow(it.currentWindowIndex, Timeline.Window()).windowStartTimeMs + it.currentPosition
@@ -33,4 +31,8 @@ fun getExoplayerPdtTime(player: () -> SimpleExoPlayer?): Long {
             }
         }
     } ?: 0
+}
+
+interface PlayerGetter {
+    fun getPlayer(): SimpleExoPlayer?
 }
