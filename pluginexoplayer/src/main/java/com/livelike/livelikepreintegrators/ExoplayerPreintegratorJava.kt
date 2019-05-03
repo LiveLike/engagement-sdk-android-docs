38628c9d4a97a9f84cbf5303d900b8b2cefb1089
Willis Pinaud <willis@livelikevr.com>
2019-05-03 08:59:05 +0200

    SDK-248: rename playerGetter to playerProvider

--- pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegratorJava.kt
+++ pluginexoplayer/src/main/java/com/livelike/livelikepreintegrators/ExoplayerPreintegratorJava.kt
@@ -9,20 +9,20 @@ import com.livelike.livelikesdk.LiveLikeSDK
  * This extension act as a plugin for Exoplayer.
  * It retrieves the timecode from an HLS stream using the PDT tags.
  *
- * @param playerGetter An interface returning the latest player instance when called.
+ * @param playerProvider An interface returning the latest player instance when called.
  * @param programId The program to connect the session with. This Id can be created from the Engagement CMS.
  */
 fun LiveLikeSDK.createExoplayerSession(
-    playerGetter: PlayerGetter,
+    playerProvider: PlayerProvider,
     programId: String
 ): LiveLikeContentSession {
     return this.createContentSession(programId) {
-        getExoplayerPdtTime(playerGetter)
+        getExoplayerPdtTime(playerProvider)
     }
 }
 
-fun getExoplayerPdtTime(playerGetter: PlayerGetter): Long {
-    return playerGetter.getPlayer()?.let {
+fun getExoplayerPdtTime(playerProvider: PlayerProvider): Long {
+    return playerProvider.get()?.let {
         it.currentTimeline?.run {
             if (!isEmpty) {
                 getWindow(it.currentWindowIndex, Timeline.Window()).windowStartTimeMs + it.currentPosition
@@ -33,6 +33,6 @@ fun getExoplayerPdtTime(playerGetter: PlayerGetter): Long {
     } ?: 0
 }
 
-interface PlayerGetter {
-    fun getPlayer(): SimpleExoPlayer?
+interface PlayerProvider {
+    fun get(): SimpleExoPlayer?
 }
