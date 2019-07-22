dba42a6e24d26686e3a468e2e522fe5271e19a2d
Willis Pinaud <willis@livelikevr.com>
2019-07-22 09:41:43 +0200

    ANDSDK-220: Fix min widget container size using OnMeasure

--- engagementsdkcore/src/main/java/com/livelike/livelikesdk/widget/view/WidgetView.kt
+++ engagementsdkcore/src/main/java/com/livelike/livelikesdk/widget/view/WidgetView.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import com.livelike.engagementsdkapi.LiveLikeContentSession
+import com.livelike.livelikesdk.utils.AndroidResource
+import com.livelike.livelikesdk.utils.logError
 
 class WidgetView(context: Context, attr: AttributeSet) : FrameLayout(context, attr) {
     fun setSession(session: LiveLikeContentSession) {
         session.setWidgetContainer(this)
     }
-    fun pause() {
-    }
-    fun resume() {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthDp = AndroidResource.pxToDp(width)
+        if(widthDp < 292 && widthDp!=0){
+            logError { "[CONFIG ERROR] Current WidgetView Width is $widthDp, it must be more than 292dp or won't display on the screen." }
+            setMeasuredDimension(0,0)
+            return
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     }
 }
