a974a3c69f97ad743052de1d25b9296e68f1d04f
Willis Pinaud <willis@livelike.com>
2019-08-21 18:35:51 +0530

    ANDSDK-330: Test Application Nickname setting

--- livelikedemo/src/main/java/com/livelike/livelikedemo/MainActivity.kt
+++ livelikedemo/src/main/java/com/livelike/livelikedemo/MainActivity.kt
@@ -5,12 +5,15 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import kotlin.reflect.KClass
 import kotlinx.android.synthetic.main.activity_main.chat_only_button
 import kotlinx.android.synthetic.main.activity_main.events_button
 import kotlinx.android.synthetic.main.activity_main.events_label
 import kotlinx.android.synthetic.main.activity_main.layout_overlay
 import kotlinx.android.synthetic.main.activity_main.layout_side_panel
+import kotlinx.android.synthetic.main.activity_main.nicknameText
 import kotlinx.android.synthetic.main.activity_main.widgets_only_button
 
 class MainActivity : AppCompatActivity() {
@@ -41,6 +44,20 @@ class MainActivity : AppCompatActivity() {
             channelManager.hide()
             events_label.text = it.name
         }
+
+        nicknameText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                getSharedPreferences("test-app", Context.MODE_PRIVATE).edit().apply {
+                    putString("UserNickname", p0?.trim().toString())
+                }.apply()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
 
         widgets_only_button.setOnClickListener { startActivity(Intent(this, WidgetOnlyActivity::class.java)) }
         chat_only_button.setOnClickListener { startActivity(Intent(this, ChatOnlyActivity::class.java)) }
