658d124ac18d2af2af9ea24a702aec1945190db4
Willis Pinaud <willis@livelike.com>
2019-08-26 16:43:16 +0530

    ANDSDK-362: Hotfix chat scrolling offset was wrong

--- engagementsdkcore/src/main/java/com/livelike/livelikesdk/chat/ChatView.kt
+++ engagementsdkcore/src/main/java/com/livelike/livelikesdk/chat/ChatView.kt
@@ -317,7 +317,7 @@ class ChatView(context: Context, attrs: AttributeSet?) : ConstraintLayout(contex
     private fun snapToLive() {
         chatdisplay?.let { rv ->
             viewModel?.chatAdapter?.itemCount?.let {
-                rv.smoothScrollToPosition(it - 1)
+                rv.smoothScrollToPosition(it)
             }
         }
     }
