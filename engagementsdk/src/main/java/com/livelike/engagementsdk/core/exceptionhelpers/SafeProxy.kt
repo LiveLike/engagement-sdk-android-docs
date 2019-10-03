83331951e7f2c5bb744182b2bc28c44d03b747bd
Shivansh Mittal <shivansh@livelike.com>
2019-10-03 15:01:08 +0530

    ANDSDK-476 :  Dynamic api proxy added handling for no arguments in methods and get target object  from poxy before casting it.

--- engagementsdk/src/main/java/com/livelike/engagementsdk/core/exceptionhelpers/SafeProxy.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/core/exceptionhelpers/SafeProxy.kt
@@ -4,29 +4,35 @@ import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 
-/**
- *
- */
-class SafeInvocationHandler(private val target: Any) : InvocationHandler {
+internal class SafeInvocationHandler(val target: Any) : InvocationHandler {
 
     override fun invoke(p0: Any?, method: Method, args: Array<out Any>?): Any {
-        return if (method.returnType.name == "void" || method.returnType == Unit.javaClass) {
+        return if (method.returnType == null || method.returnType.name == "void" || method.returnType == Unit.javaClass) {
             try {
-                method.invoke(target, args)
+                if (args?.isNotEmpty() == true) method.invoke(target, args) else method.invoke(target)
             } catch (ex: Throwable) {
                 BugsnagClient.client?.notify(ex.cause ?: ex)
                 ex.cause?.printStackTrace() ?: ex.printStackTrace()
             }
-        } else
-            method.invoke(target, args)
+        } else if (args?.isNotEmpty() == true) method.invoke(target, args) else method.invoke(target)
     }
 }
 
-fun <T : Any> T.safeProxyForEmptyReturnCalls(): T {
+internal fun <T : Any> T.safeProxyForEmptyReturnCalls(): T {
 
     val loader = javaClass.classLoader
     val classes = javaClass.interfaces
     return Proxy.newProxyInstance(
         loader, classes, SafeInvocationHandler(this)
     ) as T
+//    return this
+}
+
+internal fun <T> T.getTargetObject(): Any? {
+    val clazz = (this as Object).getClass()
+    return if (Proxy.isProxyClass(clazz)) {
+        (Proxy.getInvocationHandler(this) as SafeInvocationHandler).target
+    } else {
+        null
+    }
 }
