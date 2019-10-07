ee6af11deea20652ff47e7751c2f159c805cfee1
Shivansh Mittal <shivansh@livelike.com>
2019-10-08 02:14:48 +0530

    ANDSDK-239:Image_slider_UI

---
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/view/imageslider/EmojiSliderWidgetView.kt
@@ -0,0 +1,7 @@
+package com.livelike.engagementsdk.widget.view.imageslider
+
+import android.content.Context
+import android.util.AttributeSet
+import com.livelike.engagementsdk.widget.SpecifiedWidgetView
+
+class EmojiSliderWidgetView(context: Context, attr: AttributeSet? = null) : SpecifiedWidgetView(context, attr)
