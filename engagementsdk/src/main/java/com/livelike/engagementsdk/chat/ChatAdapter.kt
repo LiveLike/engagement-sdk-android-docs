e4aeb82a8668faa198bcbc9766e45ce2c4ec2e46
Shivam Verma <shivam@livelike.com>
2020-01-02 11:10:04 +0530

    ANDSDK-648 Update variable

--- engagementsdk/src/main/java/com/livelike/engagementsdk/chat/ChatAdapter.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/chat/ChatAdapter.kt
@@ -108,9 +108,8 @@ internal class ChatRecyclerAdapter(
 
         override fun onLongClick(p0: View?): Boolean {
             if (isPublicChat) {
-                val reactionsCount = chatReactionRepository.reactionList?.size ?: 0
                 val isOwnMessage = (p0?.tag as ChatMessage?)?.isFromMe ?: false
-                val reactionsAvailableOnMyMessage = isOwnMessage && reactionsCount > 0
+                val reactionsAvailableOnMyMessage = isOwnMessage && (chatReactionRepository.reactionList?.size ?: 0) > 0
                 if (reactionsAvailableOnMyMessage || !isOwnMessage)
                     showFloatingUI(isOwnMessage, message?.myReaction)
             }
