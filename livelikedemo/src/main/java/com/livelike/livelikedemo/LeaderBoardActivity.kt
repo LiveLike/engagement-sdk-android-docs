b64687ca8b518077a799406ac282d6d2ff284a33
Shivam Verma <shivam@livelikevr.com>
2020-08-05 11:35:34 +0530

    ES-610 remove test code

--- livelikedemo/src/main/java/com/livelike/livelikedemo/LeaderBoardActivity.kt
+++ livelikedemo/src/main/java/com/livelike/livelikedemo/LeaderBoardActivity.kt
@@ -88,7 +88,7 @@ class LeaderBoardActivity : AppCompatActivity() {
         rcyl_leader_board_entries.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 //        ed_txt_program_id.setText("47c14e1d-5786-401e-a850-22c5a91a5399") //QA
-        ed_txt_program_id.setText("6834f1fd-f24d-4538-ba51-63544f9d78eb")//Prod
+//        ed_txt_program_id.setText("6834f1fd-f24d-4538-ba51-63544f9d78eb")//Prod
         rcyl_leader_board_entries.adapter = adapter
         btn_fetch.setOnClickListener {
             val programId = ed_txt_program_id.text.toString()
