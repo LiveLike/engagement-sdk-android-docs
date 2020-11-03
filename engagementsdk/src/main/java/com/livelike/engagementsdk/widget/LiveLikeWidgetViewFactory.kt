eb3762ca4752daac0eec5e2bcbbd42623333a8e2
shivansh mittal <shivansh@livelike.com>
2020-11-03 18:57:06 +0530

    ES-1024 : added alert widget model api.

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/LiveLikeWidgetViewFactory.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/LiveLikeWidgetViewFactory.kt
@@ -1,12 +1,16 @@
 package com.livelike.engagementsdk.widget
 
 import android.view.View
+import com.livelike.engagementsdk.widget.viewModel.AlertWidgetModel
 import com.livelike.engagementsdk.widget.viewModel.CheerMeterWidgetmodel
 
 interface LiveLikeWidgetViewFactory {
 
 
     fun createCheerMeterView(cheerMeterWidgetModel: CheerMeterWidgetmodel
+    ) : View?
+
+    fun createAlertWidgetView(alertWidgetModel: AlertWidgetModel
     ) : View?
 
 
