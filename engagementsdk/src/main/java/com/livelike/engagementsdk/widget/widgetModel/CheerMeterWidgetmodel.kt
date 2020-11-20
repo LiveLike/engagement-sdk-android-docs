d7589b9cc2981514dfa59f3d1dd8103dc051c990
shivansh mittal <shivansh@livelike.com>
2020-11-20 17:08:10 +0530

    ES-1059 : code documentation added for new widget apis added

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/widgetModel/CheerMeterWidgetmodel.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/widgetModel/CheerMeterWidgetmodel.kt
@@ -6,8 +6,14 @@ import com.livelike.engagementsdk.widget.viewModel.LiveLikeWidgetMediator
 
 interface CheerMeterWidgetmodel : LiveLikeWidgetMediator {
 
+    /**
+     * live stream for vote results
+     */
     val voteResults : Stream<LiveLikeWidgetResult>
 
+    /**
+     * record the cheer vote
+     */
     fun submitVote(optionID: String)
 
 }
--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/widgetModel/QuizWidgetModel.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/widgetModel/QuizWidgetModel.kt
@@ -5,6 +5,16 @@ import com.livelike.engagementsdk.widget.model.LiveLikeWidgetResult
 import com.livelike.engagementsdk.widget.viewModel.LiveLikeWidgetMediator
 
 interface QuizWidgetModel : LiveLikeWidgetMediator {
+
+
+    /**
+    * live stream for vote results
+    */
     val voteResults: Stream<LiveLikeWidgetResult>
+
+    /**
+     * lock the answer for quiz
+     */
     fun lockInAnswer(optionID: String)
+
 }
