e2539909c0c511fbdb3a70ceabfde8770dcb6bb1
Shivam Verma <shivam@livelikevr.com>
2020-12-14 12:14:27 +0530

    ES-1178 update in stream for updating latest data variable first then send data to observers add sample in activity for handling case for intialize sdk and session in activity

--- livelikedemo/src/main/java/com/livelike/livelikedemo/TwoSessionActivity.kt
+++ livelikedemo/src/main/java/com/livelike/livelikedemo/TwoSessionActivity.kt
@@ -1,24 +1,54 @@
 package com.livelike.livelikedemo
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.two_sessions_activity.widget_session_1
-import kotlinx.android.synthetic.main.two_sessions_activity.widget_session_2
+import com.livelike.engagementsdk.EngagementSDK
+import com.livelike.engagementsdk.EpochTime
+import com.livelike.engagementsdk.LiveLikeContentSession
+import com.livelike.engagementsdk.chat.LiveLikeChatSession
+import com.livelike.engagementsdk.core.AccessTokenDelegate
+import com.livelike.engagementsdk.publicapis.ErrorDelegate
+import com.livelike.livelikedemo.channel.ChannelManager
+import kotlinx.android.synthetic.main.two_sessions_activity.chat_view
+import kotlinx.android.synthetic.main.two_sessions_activity.widget_view
 
 class TwoSessionActivity : Activity() {
+
+    private lateinit var engagementSDK: EngagementSDK
+    private lateinit var channelManager: ChannelManager
+    private lateinit var session: LiveLikeContentSession
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.two_sessions_activity)
+        val sharedPref = getSharedPreferences("app", Context.MODE_PRIVATE)
+        channelManager = (application as LiveLikeApplication).channelManager
 
-        (application as LiveLikeApplication).sdk.let {
-            widget_session_1.setSession(it.createContentSession("3d639cb0-b2c4-4f70-8875-e39c74197580"))
-        }
-        @Suppress("UNNECESSARY_SAFE_CALL")
-        (application as LiveLikeApplication).sdk2?.let {
-            widget_session_2.setSession(it.createContentSession("fef045d9-4ae4-4a87-8e47-7a5929c14cce"))
-        }
+        engagementSDK = EngagementSDK(BuildConfig.APP_CLIENT_ID, applicationContext,
+            object : ErrorDelegate() {
+                override fun onError(error: String) {
+
+                }
+            }, accessTokenDelegate = object : AccessTokenDelegate {
+                override fun getAccessToken(): String? {
+                    return sharedPref.getString("access", null)
+                }
+
+                override fun storeAccessToken(accessToken: String?) {
+                    sharedPref.edit().putString("access", accessToken).apply()
+
+                    session =
+                        engagementSDK.createContentSession(channelManager.selectedChannel.llProgram.toString())
+                    widget_view.setSession(session)
+
+                    chat_view.setSession(session.chatSession)
+
+
+                }
+            })
+
     }
 }
