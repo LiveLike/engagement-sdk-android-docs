5ef0a3c4da3edc016aa607fa84a2418ea88b3653
shivansh mittal <shivansh@livelike.com>
2021-02-17 18:58:01 +0530

    ES-1383 : fixing lint errors

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/view/SocialEmbedWidgetView.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/view/SocialEmbedWidgetView.kt
@@ -3,6 +3,7 @@ package com.livelike.engagementsdk.widget.view
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.content.ContextCompat
@@ -123,9 +124,12 @@ internal class SocialEmbedWidgetView(context: Context) : SpecifiedWidgetView(con
                     view: WebView?,
                     request: WebResourceRequest?
                 ): Boolean {
-                    Log.d("url ", "${request?.url}")
-                    request?.url?.let { url ->
-                        openLink(context, url?.toString())
+                    progress_bar.hide()
+
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        request?.url?.let { url ->
+                            openLink(context, url?.toString())
+                        }
                     }
                     return true
                 }
