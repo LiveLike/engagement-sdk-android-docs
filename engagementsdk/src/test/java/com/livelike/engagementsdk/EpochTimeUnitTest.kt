3bcbc63c012021e22f07299138b672f8ae5797f8
shivamlivelike <62872583+shivamlivelike@users.noreply.github.com>
2021-04-15 13:13:07 +0530

    ES-1695 update test case for EpochTime for testing multiple sample formats

--- engagementsdk/src/test/java/com/livelike/engagementsdk/EpochTimeUnitTest.kt
+++ engagementsdk/src/test/java/com/livelike/engagementsdk/EpochTimeUnitTest.kt
@@ -1,16 +1,29 @@
 package com.livelike.engagementsdk
 
-import org.junit.Test
+import org.junit.experimental.theories.DataPoints
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
 
+@RunWith(Theories::class)
+open class EpochTimeUnitTest {
 
-class EpochTimeUnitTest {
-
-    @Test
-    fun testISOFormats() {
-        assert("2020-09-04T16:40:53.000Z".parseISODateTime() != null)
-        assert("2020-09-04T16:40:53.000000Z".parseISODateTime() != null)
-        assert("2020-09-04T16:40:53+05:30".parseISODateTime() != null)
-        assert("2020-09-04T16:40:53.000000+00:00".parseISODateTime() != null)
-        assert("2020-09-04T16:40:53Z".parseISODateTime() != null)
+    @Theory
+    fun testISOFormats(sampleFormat: String) {
+        assert(sampleFormat.parseISODateTime() != null)
     }
+
+
+    companion object {
+        @DataPoints
+        @JvmField
+        var candidates = arrayListOf(
+            "2020-09-04T16:40:53.000Z",
+            "2020-09-04T16:40:53.000000Z",
+            "2020-09-04T16:40:53+05:30",
+            "2020-09-04T16:40:53.000000+00:00",
+            "2020-09-04T16:40:53Z"
+        )
+    }
+
 }
