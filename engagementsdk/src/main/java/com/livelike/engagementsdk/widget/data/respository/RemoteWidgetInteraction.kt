43164892f4b4b9732814f991d9e63bca65509788
shivansh mittal <shivansh@livelike.com>
2021-05-18 11:22:53 +0530

    ES-1847 : widget interaction api integration call

---
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/data/respository/RemoteWidgetInteraction.kt
@@ -0,0 +1,20 @@
+package com.livelike.engagementsdk.widget.data.respository
+
+import com.livelike.engagementsdk.core.services.network.EngagementDataClientImpl
+import com.livelike.engagementsdk.core.services.network.RequestType
+import com.livelike.engagementsdk.core.services.network.Result
+import com.livelike.engagementsdk.widget.data.models.UserWidgetInteractionApi
+
+internal class RemoteWidgetInteraction {
+
+
+    private  val engagementDataClientImpl: EngagementDataClientImpl = EngagementDataClientImpl()
+
+
+    internal suspend fun getWidgetInteractions(url:String, accessToken:String) : Result<UserWidgetInteractionApi>{
+        return engagementDataClientImpl.remoteCall<UserWidgetInteractionApi>(url, RequestType.GET, accessToken = accessToken)
+    }
+
+
+
+}
