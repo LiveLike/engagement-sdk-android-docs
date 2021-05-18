9bc26d9b08c9a2451e5da25329221b2543f03088
shivansh mittal <shivansh@livelike.com>
2021-05-18 11:23:49 +0530

    ES-1846 : widget interactions save locally in memory, [TBD : to save locally}

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/data/respository/WidgetInteractionRepository.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/data/respository/WidgetInteractionRepository.kt
@@ -1,18 +1,48 @@
 package com.livelike.engagementsdk.widget.data.respository
 
 import android.content.Context
+import com.livelike.engagementsdk.widget.data.models.WidgetKind
 import com.livelike.engagementsdk.widget.data.models.WidgetUserInteractionBase
 
 /**
  * Repository that handles user's widget interaction data. It knows what data sources need to be
  * triggered to get widget interaction and where to store the data.
-**/
-internal class WidgetInteractionRepository(val context : Context) {
+ **/
+internal class WidgetInteractionRepository(val context: Context, val programID: String) {
 
 
-    fun getWidgetInteraction(widgetId: String) : WidgetUserInteractionBase?{
-        return null
+    private val remoteWidgetInteraction: RemoteWidgetInteraction = RemoteWidgetInteraction()
+
+    private val widgetInteractionMap = mutableMapOf<String, WidgetUserInteractionBase>()
+
+    fun <T : WidgetUserInteractionBase> getWidgetInteraction(
+        widgetId: String,
+        widgetKind: WidgetKind
+    ): T? {
+        return widgetInteractionMap[widgetId] as T?
     }
 
+    internal suspend fun fetchAndStoreWidgetInteractions(url: String, accessToken: String) {
+
+        val widgetInteractionsResult =
+            remoteWidgetInteraction.getWidgetInteractions(url, accessToken)
+
+        if (widgetInteractionsResult is com.livelike.engagementsdk.core.services.network.Result.Success) {
+            val interactionList = mutableListOf<WidgetUserInteractionBase>()
+            widgetInteractionsResult.data.interactions?.let { interactions ->
+                interactions.cheerMeter?.let { interactionList.addAll(it) }
+                interactions.emojiSlider?.let { interactionList.addAll(it) }
+                interactions.textPoll?.let { interactionList.addAll(it) }
+                interactions.textPrediction?.let { interactionList.addAll(it) }
+                interactions.textQuiz?.let { interactionList.addAll(it) }
+                interactions.imagePoll?.let { interactionList.addAll(it) }
+                interactions.imagePrediction?.let { interactionList.addAll(it) }
+                interactions.imagePoll?.let { interactionList.addAll(it) }
+            }
+            interactionList.forEach {
+                widgetInteractionMap[it.id] = it
+            }
+        }
+    }
 
 }
