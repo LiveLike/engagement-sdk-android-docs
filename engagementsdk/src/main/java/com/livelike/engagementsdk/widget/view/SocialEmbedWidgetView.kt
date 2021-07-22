24781a70f75b2bc0744787fbb777e32a4b0e03bf
sumon88 <sumon@livelike.com>
2021-07-22 17:05:54 +0530

    ES-2364 bug fixes for theme

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/view/SocialEmbedWidgetView.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/view/SocialEmbedWidgetView.kt
@@ -148,6 +148,11 @@ internal class SocialEmbedWidgetView(context: Context) : SpecifiedWidgetView(con
                 }
 
             }
+
+            widgetsTheme?.let {
+                applyTheme(it)
+            }
+
         }
     }
 
