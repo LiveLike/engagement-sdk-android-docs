97709f25332e60eb889ca3aca8ca819973d3f924
sumon88 <sumon@livelike.com>
2021-09-28 15:44:54 +0530

    ES-2708 - crash fixed

--- engagementsdk/src/main/java/com/livelike/engagementsdk/widget/model/ImageSliderEntity.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/widget/model/ImageSliderEntity.kt
@@ -7,8 +7,8 @@ internal data class ImageSliderEntity(
     val initialMagnitude: Float?,
     @field:SerializedName("average_magnitude")
     val averageMagnitude: Float?,
-    @field:SerializedName("vote_url")
-    val voteUrl: String
+   /* @field:SerializedName("vote_url")
+    val voteUrl: String*/
 
 ) : Resource() {
     override fun toLiveLikeWidgetResult(): LiveLikeWidgetResult {
