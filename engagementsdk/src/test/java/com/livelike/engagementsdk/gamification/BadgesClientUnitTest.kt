41af557c1380544ab62a5969e56af75119c6fd7c
shivansh mittal <shivansh@livelilke.com>
2021-11-01 18:47:22 +0530

    ES-2751 : unit test for badge progression

--- engagementsdk/src/test/java/com/livelike/engagementsdk/gamification/BadgesClientUnitTest.kt
+++ engagementsdk/src/test/java/com/livelike/engagementsdk/gamification/BadgesClientUnitTest.kt
@@ -9,6 +9,7 @@ import com.livelike.engagementsdk.EngagementSDK
 import com.livelike.engagementsdk.chat.data.remote.LiveLikePagination
 import com.livelike.engagementsdk.core.data.models.LLPaginatedResult
 import com.livelike.engagementsdk.gamification.models.Badge
+import com.livelike.engagementsdk.gamification.models.BadgeProgress
 import com.livelike.engagementsdk.publicapis.IEngagement
 import com.livelike.engagementsdk.publicapis.LiveLikeCallback
 import okhttp3.mockwebserver.MockResponse
@@ -16,9 +17,9 @@ import okhttp3.mockwebserver.MockWebServer
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.Mockito.timeout
+import org.mockito.Mockito.verify
 import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.timeout
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
@@ -91,6 +92,37 @@ class BadgesClientUnitTest {
         verify(callback, timeout(5000)).onResponse(resultCaptor.capture(), errorCaptor.capture())
         assert(resultCaptor.firstValue.results?.isNotEmpty()?:false)
         assert(resultCaptor.firstValue.results?.get(0)?.name == "TestFree")
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun badge_progression_success_with_data() {
+        val callback =
+            Mockito.mock(LiveLikeCallback::class.java) as LiveLikeCallback<List<BadgeProgress>>
+        Thread.sleep(1000)
+        mockWebServer.enqueue(MockResponse().apply {
+            setResponseCode(200)
+            setBody(
+                javaClass.classLoader.getResourceAsStream("user_profile_resource.json").readAll()
+            )
+        })
+        mockWebServer.enqueue(MockResponse().apply {
+            setResponseCode(200)
+            setBody(
+                javaClass.classLoader.getResourceAsStream("badge_progression.json").readAll()
+            )
+        })
+        sdk.badges().getProfileBadgeProgress("e58b8f5c-01b9-4423-bc2d-80a62ce28891", mutableListOf<String>().apply{
+            add("7c2a61af-2e88-40de-8df9-027127a6a1e2")
+        }, callback)
+        val resultCaptor =
+            argumentCaptor<List<BadgeProgress>>()
+        val errorCaptor =
+            argumentCaptor<String>()
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
+        verify(callback, timeout(5000)).onResponse(resultCaptor.capture(), errorCaptor.capture())
+        assert(resultCaptor.firstValue.isNotEmpty()?:false)
+        assert(resultCaptor.firstValue[0]?.badge.name == "TestFree")
         mockWebServer.shutdown()
     }
 
