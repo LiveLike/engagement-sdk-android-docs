ccd121222b1b20d322b82ec6c0180a1a790bea12
shivansh mittal <shivansh@livelilke.com>
2021-11-15 09:39:56 +0530

    ES-2863 : changing interface to abstract class to enable future additions on rewardsEventListener

--- engagementsdk/src/main/java/com/livelike/engagementsdk/gamification/RewardEventListener.kt
+++ engagementsdk/src/main/java/com/livelike/engagementsdk/gamification/RewardEventListener.kt
@@ -4,7 +4,7 @@ package com.livelike.engagementsdk.gamification
  * all events related to rewards like reward transfer received will be surfaced onto this listener
  * integrators can register this listener on a instance of rewardClient that is equivalent to sdk.rewards()
  **/
-interface RewardEventsListener {
+abstract class RewardEventsListener {
 
     /**
      * This is executed whenever the user receives a reward transfer
@@ -12,7 +12,9 @@ interface RewardEventsListener {
      **/
     fun onReceiveNewRewardItemTransfer(
         rewardItemTransfer: TransferRewardItem
-    )
+    ){
+        return
+    }
 }
 
 
