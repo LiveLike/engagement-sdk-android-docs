577e7e4deb2b3be2361cfb101d1d425ac7535ec1
shivamlivelike <62872583+shivamlivelike@users.noreply.github.com>
2022-02-16 15:53:48 +0530

    ES-3203 remove formatter from exoplayer added default for now

--- livelikedemo/src/main/java/com/livelike/livelikedemo/ExoPlayerActivity.kt
+++ livelikedemo/src/main/java/com/livelike/livelikedemo/ExoPlayerActivity.kt
@@ -545,7 +545,7 @@ class ExoPlayerActivity : AppCompatActivity() {
             if (showLink) {
                 chat_view.chatMessageUrlPatterns = customLink
             }
-            chat_view.reactionCountFormatter = { count -> prettyCount(count)}
+//            chat_view.reactionCountFormatter = { count -> prettyCount(count)}
 
             chat_view.chatViewDelegate = object : ChatViewDelegate {
                 override fun onCreateViewHolder(
@@ -619,20 +619,6 @@ class ExoPlayerActivity : AppCompatActivity() {
 
     private fun stopThemeRandomizer() {
         themeRadomizerHandler.removeCallbacksAndMessages(null)
-    }
-
-    private fun prettyCount(number: Number): String {
-        val suffix = charArrayOf(' ', 'k', 'M', 'B', 'T', 'P', 'E')
-        val numValue = number.toLong()
-        val value = floor(log10(numValue.toDouble())).toInt()
-        val base = value / 3
-        return if (value >= 3 && base < suffix.size) {
-            DecimalFormat("#0.0").format(
-                numValue / 10.0.pow((base * 3).toDouble())
-            ) + suffix[base]
-        } else {
-            DecimalFormat("#,##0").format(numValue)
-        }
     }
 
     private fun playThemeRandomizer() {
